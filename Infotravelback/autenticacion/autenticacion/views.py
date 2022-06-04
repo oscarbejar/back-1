857792b91fcc400a128a86589063db209a7e205e
leo714 <leocampolongo@hotmail.com>
2022-06-04 17:30:31 -0300

    registro correcciones

--- Infotravelback/autenticacion/autenticacion/views.py
+++ Infotravelback/autenticacion/autenticacion/views.py
@@ -11,7 +11,7 @@ class VRegistro(View):
     def get(self,request): #el get nos muestra el formulario
 
         form=UserCreationForm() #variable que almacena el formulario a ver
-        return render(request, "registro/registro.html",{"form":form})
+        return render(request, "../../../../Front/src/components/FormRegistro.jsx",{"form":form})
     
     def post(self,request):    #gestiona el envio de info del formulario
         form=UserCreationForm(request.POST)   #almacena el form con la info que el user haya introducido
@@ -22,8 +22,8 @@ class VRegistro(View):
        
             login(request, usuario) #una vez realizado el registro hace un login automatico y redirecciona al home
 
-            return redirect('home')
+            return redirect('../../../../Front/src/pages/PaginaPrincipal.jsx')
         else:
             for msg in form.error_messages:
                 messages.error(request, form.error_messages[msg])
-            return render(request,"registro/registro.html",{"form":form})
+            return render(request,"../../../../Front/src/components/FormRegistro.jsx",{"form":form})
