a5767edea51f779177d6746328b97559a0184489
infotravelifts16 <83845553+infotravelifts16@users.noreply.github.com>
2022-08-04 15:22:04 -0300

    Update settings.py

--- back/settings.py
+++ back/settings.py
@@ -146,7 +146,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'infotravelifts16@gmail.com'
-EMAIL_HOST_PASSWORD = 'abqucnilwwbcouze'
+EMAIL_HOST_PASSWORD = ''
 
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
